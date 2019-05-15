bd6b0cb7288c2a6d703219944c55930c7b6744a9
pj <734552377@qq.com>
2019-05-15 17:10:00 +0800

    基本界面搭建

--- app/src/main/java/com/ucast/taxiscreen/activities/KeybordTestActivity.java
+++ app/src/main/java/com/ucast/taxiscreen/activities/KeybordTestActivity.java
@@ -2,11 +2,18 @@ package com.ucast.taxiscreen.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.ucast.taxiscreen.R;
 
 public class KeybordTestActivity extends AppCompatActivity {
-
+    TextView k1_tv;
+    TextView k2_tv;
+    TextView k3_tv;
+    TextView k4_tv;
+    TextView k5_tv;
+    TextView k6_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
